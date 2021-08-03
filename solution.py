881427335aaac6ad2dcc518bf932b94fbbd75f9b
Colin Wagner <colin.d.wagner@gmail.com>
2021-08-03 12:00:04 -0500

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        answer = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return answer
