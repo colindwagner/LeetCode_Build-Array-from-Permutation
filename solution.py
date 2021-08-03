daebd946b1075e696c1dfd10d43cfe69030e0477
Colin Wagner <colin.d.wagner@gmail.com>
2021-08-03 12:00:40 -0500

    Update solution.py
    typo

--- solution.py
+++ solution.py
@@ -2,5 +2,5 @@ class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         answer = []
         for i in range(len(nums)):
-            ans.append(nums[nums[i]])
+            answer.append(nums[nums[i]])
         return answer
